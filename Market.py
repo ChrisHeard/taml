ee1e8e818caaf9da9989d0068361a6ec0739ffe5
Christopher Heard <christopher.c.heard+git@gmail.com>
2024-06-14 16:23:04 +0100

    "removed legacy"

--- Market.py
+++ Market.py
@@ -12,11 +12,11 @@ class Market(BaseModel):
     name: str
     api_ticker: str
     suffix: str
-    index_hash: bytes
-    components_hash: bytes
-    link_hash: bytes
+    index_hash: str
+    components_hash: str
+    link_hash: str
     link_ref: List[str]
-    client: Redis
+    redis: Redis
 
     def __init__(self, id: str):
 
@@ -30,26 +30,31 @@ class Market(BaseModel):
         self.redis = utils.load_redis()
         self.updater = MarketUpdater(market=self, url=id_dict['url'], parser=id_dict['parser'])
         self.logger = CustomLogger()
-
         self.name = id_dict['name']
         self.api_ticker = id_dict['api_ticker']
         self.suffix = id_dict['suffix']
-        self.index_hash = f'index:{self.id}'.encode('utf-8')
-        self.components_hash = f'components:{self.id}'.encode('utf-8')
-        self.link_hash = f'links:{self.id}'.encode('utf-8')
+        self.index_hash = f'index:{self.id}'
+        self.components_hash = f'components:{self.id}'
+        self.link_hash = f'links:{self.id}'
         self.adjustments = id_dict['adjustments']
         self.link_ref = self.__get_link_ref()
 
-        def __get_link_ref(self) -> Optional[List[str]]:
+    def __get_link_ref(self) -> Optional[List[str]]:
 
-            links_dict: dict = self.redis.hgetall(self.link_hash)
+        links_dict: dict = self.redis.hgetall(self.link_hash)
 
-            if not links_dict:
-                self.logger.info(f'{self.link_hash} returned an empty dict and requires initializing. Use MarketUpdater to initialize.')
-                return None
-            
-            max_key = max(links_dict.keys(), key=int)
+        if not links_dict:
+            self.logger.info(f'{self.link_hash} returned an empty dict and requires initializing. Use MarketUpdater to initialize.')
+            return None
+        
+        max_key = max(links_dict.keys(), key=int)
 
-            latest_list = utils.bytes_to_py(links_dict.get(max_key))
+        latest_list = utils.bytes_to_py(links_dict.get(max_key))
 
-            return latest_list
+        return latest_list
+    
+
+    # Pydantic configuration class
+    class Config:
+
+        arbitrary_types_allowed = True
